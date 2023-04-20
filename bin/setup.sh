b69153cec42a10073aa9c2d6094fe95db50d2a87
Skip Baney <twelvelabs@gmail.com>
2023-04-19 23:29:48 -0500

    chore: add repo setup checks

--- bin/setup.sh
+++ bin/setup.sh
@@ -33,3 +33,29 @@ fi
 if ! command -v pin-github-action >/dev/null 2>&1; then
     npm install -g pin-github-action
 fi
+
+# Ensure local repo
+if ! git rev-parse --is-inside-work-tree &>/dev/null; then
+    if gum confirm "Create local git repo?"; then
+        set -o xtrace
+        git init
+        git add .
+        git commit -m "feat: initial commit"
+        set +o xtrace
+    fi
+fi
+
+# Ensure remote repo
+if ! gh repo view --json url &>/dev/null; then
+    if gum confirm "Create remote git repo?"; then
+        set -o xtrace
+        gh repo create
+        sleep 1
+        git remote set-head origin --auto
+        set +o xtrace
+
+        echo ""
+        echo "Remote repo created: $(gh repo view --json url --jq .url)"
+        echo ""
+    fi
+fi
