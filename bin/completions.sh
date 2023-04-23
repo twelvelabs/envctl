3d740f6ae5c82ea407ad454813f7838250723558
Skip Baney <twelvelabs@gmail.com>
2023-04-23 00:08:02 -0500

    refactor: optimize pre-build scripts

--- bin/completions.sh
+++ bin/completions.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 set -o errexit -o errtrace -o nounset -o pipefail
 
+app="envctl"
+go build -o "build/$app" .
+
 rm -rf build/completions
 mkdir -p build/completions
 
 # Generate Cobra shell completion scripts.
 # See: https://github.com/spf13/cobra/blob/main/shell_completions.md
 for sh in bash zsh fish; do
-    go run . completion "$sh" >"build/completions/envctl.$sh"
+    "build/$app" completion "$sh" >"build/completions/$app.$sh"
 done
--- bin/manpages.sh
+++ bin/manpages.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -o errexit -o errtrace -o nounset -o pipefail
 
+app="envctl"
+go build -o "build/$app" .
+
 rm -rf build/manpages
 mkdir -p build/manpages
 
 # Generate Cobra man pages.
 # See: https://github.com/spf13/cobra/blob/main/doc/man_docs.md
-go run . man | gzip -c -9 >build/manpages/envctl.1.gz
+"build/$app" man | gzip -c -9 >build/manpages/envctl.1.gz
